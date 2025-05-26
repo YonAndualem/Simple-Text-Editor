e3a25bf4fa9a42922c0b7288ff6854dbac5c2ef1
Yonas B.Andualem <yonasandualem1472@gmail.com>
2025-05-26 11:18:35 +0300

    Remove echo when typing

--- Code.asm
+++ Code.asm
@@ -1,22 +1,22 @@
-org 100h              ; COM file starts at offset 100h
+org 100h
 
 mov ah, 09h
 mov dx, offset intro_msg
-int 21h               ; Display intro message
+int 21h           ; Display message
 
 start_loop:
-    mov ah, 01h       ; Function to read character from keyboard
-    int 21h
-    cmp al, 27        ; ASCII 27 = ESC key
+    mov ah, 00h
+    int 16h       ; Read character from keyboard (no echo)
+    cmp al, 27    ; ESC key?
     je exit_editor
 
-    mov ah, 0Eh       ; BIOS teletype output
-    int 10h           ; Show character
+    mov ah, 0Eh
+    int 10h       ; Manually print character
 
     jmp start_loop
 
 exit_editor:
-    mov ah, 4Ch       ; Terminate program
+    mov ah, 4Ch
     int 21h
 
 intro_msg db 'Simple Text Editor - Press ESC to Exit$', 0
